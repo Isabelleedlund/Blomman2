109c88b238bc220aedda8d97cdc95ecee7d31797
Isabelle Edlund <edlund.isabelle@gmail.com>
2019-12-09 11:47:56 +0100

    Update footer, navbar and title on Checkout page.
    Jag har gjort en push nu och det som gjorts är:
    - Samma fot på alla sidor
    - JS filen uppdaterad med footern i alla filer (Däremot funkar inte min kod på startsidan för ikonerna. Kollar på det sen 🙂
    
    - Lagt in samma Navbar på Checkout som confirmation. (Utan cart ikon)
    
    - Uppdaterat Titel i Checkout så nu står det BEKRÄFTA KÖP och BESTÄLL
    
    - Har även kommenterat i Html så det är en struktur så man ser var navbar, content, footer börjar och slutar.

--- js/main.js
+++ js/main.js
@@ -218,4 +218,32 @@ function addToCart(event) {
         console.log(cart);
 };
 
+// Footer Start \\
 
+// Change Facebook Icon color when hover over img. 
+$('#imgfb').hover(
+    function(){
+        $(this).attr('src','../img/002-facebook_color.png')
+    },
+    function(){
+        $(this).attr('src','../img/002-facebook_grey.png')
+    });
+
+// Change Instagram Icon color when hover over img. 
+$('#imgig').hover(
+    function(){
+        $(this).attr('src','../img/003-instagram_color.png')
+    },
+    function(){
+        $(this).attr('src','../img/003-instagram_grey.png')
+    });
+
+// Change Twitter Icon color when hover over img. 
+$('#imgtwi').hover(
+    function(){
+        $(this).attr('src','../img/005-twitter_color.png')
+    },
+    function(){
+        $(this).attr('src','../img/005-twitter_grey.png')
+});
+// Footer End \\
