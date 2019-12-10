b0435514fe669659c3accbc615362de966c34567
Balthazar Berling Kelpe <balthazarberling@gmail.com>
2019-12-10 12:40:14 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -30,8 +30,6 @@ products.push(products1, products2, products3, products4, products5, products6);
  
 let cart = JSON.parse(localStorage.getItem("cart")) || [];
 
-// let cart = JSON.parse(localStorage.getItem("cart")) || localStorage.setItem("cart", JSON.stringify([]));
-
 $(document).ready(function() {
 
     createCart();
