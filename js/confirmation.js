60697791aa548aa135e68706330230a72e71db01
Isabelle Edlund <edlund.isabelle@gmail.com>
2019-12-10 15:55:00 +0100

    update scss files

--- js/confirmation.js
+++ js/confirmation.js
@@ -1,13 +1,56 @@
 $(document).ready(function() { 
 
+min = 10000;
+// max
+
 // Generate a order Number when 
-let buyOrder = localStorage.getItem('orderNo');
-    if(buyOrder == null){
-        buyOrder = Math.floor(Math.random() * 99999999)
+let buyOrder = localStorage.getItem('orderNumber', '');
+
+    // if(buyOrder == null){
+        buyOrder = Math.round(1) * (min + 3) + min;
+        // buyOrder = Math.floor(Math.random() * 10000) +1;
         localStorage.setItem('orderNo', buyOrder);
-    }
+    
     $("#on").text(buyOrder)
 });
+
+
+
+// function Range(begin, end) {
+//     this.low = begin;
+//     this.hi = end;
+//     this.has = function(n) {
+//        return this.low <= n <= this.hi;
+//     }
+//   }
+  
+//   // code example
+//   var range = new Range(1,100);
+//   var num = 5;
+  
+//   if (range.has(num)) {
+//     alert("Number in range");
+//   }
+
+
+// let numbers = []
+
+// for (let i = 1; i <= 100; i++) {
+//     numbers.push(i)
+//     console.log(numbers[i])
+// }
+
+// let orderNumber = {
+//                 from: 10000,
+//                 to: 99999
+// };
+
+// for(let i = 0; i < orderNumber.length; i++) { 
+//     console.log(orderNumber[i]);
+// }
+
+
+
 
 // Change Facebook Icon color when hover over img. 
 $('#imgfb').hover(
@@ -35,3 +78,5 @@ $('#imgtwi').hover(
     function(){
         $(this).attr('src','../img/005-twitter_grey.png')
 });
+
+// });
